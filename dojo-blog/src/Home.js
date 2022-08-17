0a2a6d8baaa86e456982e1c175d4dab9a0037cec
unknown <ajfoucaultmo@mail.usf.edu>
2022-08-16 20:13:23 -0700

    #8 - Using State (useState hook)

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,23 +1,22 @@
-const Home = () => {
-  const handleClick = (e) => {
-    console.log('Hello Ninjas', e);
-  };
+import { useState } from 'react';
 
-  const handleClickAgain = (name, e) => {
-    console.log(`Hello, ${name}, ${e.target}`);
+const Home = () => {
+  // let name = 'mario';
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    setName('luigi');
+    setAge(30);
   };
 
   return (
     <div className='home'>
       <h2>Homepage</h2>
+      <p>
+        {name} is {age} years old.
+      </p>
       <button onClick={handleClick}>Click Me!</button>
-      <button
-        onClick={(e) => {
-          handleClickAgain('Mario', e);
-        }}
-      >
-        Click me again
-      </button>
     </div>
   );
 };
